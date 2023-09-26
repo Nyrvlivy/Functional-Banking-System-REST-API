bb534f692a9f7e8b66ab91fbe025591274ed090c
Nyrvlivy <nyrvlivy@gmail.com>
2023-09-26 18:32:58 -0300

    Updated all from Portuguese to English.

---
+++ src/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bank: {
+        name: 'Cubos Bank',
+        number: '123',
+        branchNumber: '0001',
+        password: 'Cubos123Bank'
+    },
+    accounts: [],
+    withdraws: [],
+    deposits: [],
+    transfers: []
+}
