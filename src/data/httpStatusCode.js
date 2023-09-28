edbc74c92d134a572f8e2bc149cd2ace7db2c2a2
Nyrvlivy <nyrvlivy@gmail.com>
2023-09-28 20:27:02 -0300

    Added 'httpStatusCodes.js' and updated files with HTTP status codes.

---
+++ src/data/httpStatusCode.js
@@ -0,0 +1,10 @@
+module.exports = {
+  200: "OK: Successful request!",
+  201: "Created: Successful request, and something was created!",
+  204: "No Content: Successful request, no content in the response body!",
+  400: "Bad Request: The server didn't understand the request due to invalid syntax/format!",
+  401: "Unauthorized: The user is not authenticated (logged in)!",
+  403: "Forbidden: The user doesn't have permission to access the requested resource!",
+  404: "Not Found: The server couldn't find the requested resource!",
+  500: "Internal Server Error: Failures caused by the server!",
+};
--- src/middlewares/bankPasswordValidation.js
+++ src/middlewares/bankPasswordValidation.js
@@ -1,14 +1,15 @@
 const database = require("../data/database");
+const httpStatusCode = require("../data/httpStatusCode");
 
 const validateBankPassword = (req, res, next) => {
   const { bank_password } = req.query;
 
   if (!bank_password) {
-    return res.status(400).json({ message: "Blank password not allowed!" });
+    return res.status(400).json({ message: httpStatusCode[400] });
   }
 
   if (bank_password !== database.bank.password) {
-    return res.status(401).json({ message: "Invalid password!" });
+    return res.status(401).json({ message: httpStatusCode[401] });
   }
 
   next();
