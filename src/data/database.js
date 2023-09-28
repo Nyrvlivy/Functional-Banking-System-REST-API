0b29e45841dc83cf5232c15fa404d4422872e453
Nyrvlivy <nyrvlivy@gmail.com>
2023-09-28 19:47:16 -0300

    First complete endpoint: List existing bank accounts.

--- src/data/database.js
+++ src/data/database.js
@@ -6,7 +6,30 @@ module.exports = {
     password: "Cubos123Bank",
   },
   accounts: [
-
+    {
+      number: "1",
+      balance: 0,
+      user: {
+        name: "Foo Bar",
+        cpf: "00011122233",
+        birthDate: "2021-03-15",
+        phoneNumber: "71999998888",
+        email: "foo@bar.com",
+        password: "1234",
+      },
+    },
+    {
+      number: "2",
+      balance: 1000,
+      user: {
+        name: "Foo Bar 2",
+        cpf: "00011122234",
+        birthDate: "2021-03-15",
+        phoneNumber: "71999998888",
+        email: "foo@bar2.com",
+        password: "12345",
+      },
+    },
   ],
 
   withdraws: [],
